f8f8faa08c616ebdb16874b49e63921641200612
Josiah Atkinson <josiah.atkinson96@gmail.com>
2019-08-04 10:24:49 +1000

    Adding function to get Grifith Auth token for APIs
    Creating function to get auth token from user SSO cookie so that it can
    be used to get auth token that allows users to access information.
    
    * Auth token retriaval function
    * Getting study details from mycourses/v1 API route

--- src/app/study-tab/study-tab.page.ts
+++ src/app/study-tab/study-tab.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-study-tab',
@@ -7,6 +8,46 @@ import { Component } from '@angular/core';
 })
 export class StudyPage {
 
-  constructor() {}
+  studentCookie: string = 'T1RLAQLLc3-31-5oxIhVchYcpZqOPQEbVxDTqkdTzVvhcQU5aVEDq-2-AABw4KzdVHaqqqEyBnUGXWQm6NngAK1alP4NxYfA7SzMFY2wcENco2dG6aUm-PoU5ufW0Jz6EWrnZsMJCt5lQe6C61NfiKSVRsXg1POLAtV23ulAiV7ze0Zn1bxFg9jUgF0DmyfNJJqFhaHZLXmO-4Jsfg**';
+  griffithToken: string;
+  courseData = [];
+
+  constructor(private http: HttpClient) {}
+
+  // createAuthHeader() {
+  //   var authHeaders = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded').set('Cache-Control', 'no-cache');
+  //   return headers
+  // }
+
+  getGriffithToken() {
+    let url = 'https://www108.griffith.edu.au/gatekeeper-api/token';
+    let postBody = 'client_id=web_portal&grant_type=urn:gu:oauth:grant-type:sso&assertion=' + this.studentCookie;
+    var authHeaders = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded').set('Cache-Control', 'no-cache');
+    this.http.post(url, postBody, {headers: authHeaders}
+    ).subscribe((response) => {
+      this.griffithToken = response["access_token"];
+      console.log(this.griffithToken)
+      // this needs to be moved out of this function! Just using for hackathon
+      this.getGriffithData(this.griffithToken, 'https://www108.griffith.edu.au/gatekeeper-api/mycourses/v1')
+    });
+  }
+
+  getGriffithData(token, url) {
+    var options = new HttpHeaders().set('Content-Type', 'application/json').set('Authorization', 'bearer ' + token);
+    this.http.get(url, {headers: options}).subscribe((response: any[]) => {
+      this.courseData = response['programs'][0]['teachingPeriods'][0]['courses'];
+      // for (var i in courses) {
+      //   this.courseData.push({
+      //     name: courses[i]['name'],
+      //     code: courses[i]['code']
+      //   });
+      // }
+      console.log(this.courseData);
+    });
+  }
+
+  ngOnInit() {
+    this.getGriffithToken()
+  }
 
 }
